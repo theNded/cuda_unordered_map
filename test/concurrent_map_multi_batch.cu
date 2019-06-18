88a75a2086923799b5f27b614f649ebd49486d57
Wei Dong <weidong@andrew.cmu.edu>
2019-06-18 14:52:39 -0700

    formalize test cases

--- test/concurrent_map_multi_batch.cu
+++ test/concurrent_map_multi_batch.cu
@@ -38,54 +38,49 @@ int main(int argc, char** argv) {
     }
     printf("Device: %s\n", devProp.name);
 
-    /** Hash table meta data **/
-    uint32_t num_keys = 1 << 20;
-
+    /******** Hash table meta data ********/
+    uint32_t num_elems = 1 << 20;
     float expected_chain = 0.6f;
     uint32_t num_elems_per_bucket = 100;
     uint32_t expected_elems_per_bucket = expected_chain * num_elems_per_bucket;
-    uint32_t num_buckets = (num_keys + expected_elems_per_bucket - 1) /
+    uint32_t num_buckets = (num_elems + expected_elems_per_bucket - 1) /
                            expected_elems_per_bucket;
 
-    /** Generate key-value pairs for building **/
-    float existing_ratio = 0.6f;
-    uint32_t num_queries = num_keys;
-    auto num_elements = 2 * num_keys;
-
+    /******** Insertion data ********/
     using KeyT = uint32_t;
     using ValueT = uint32_t;
     const auto f = [](const KeyT& key) { return key * 10; };
 
-    std::vector<KeyT> h_key(num_elements);
-    std::vector<ValueT> h_value(num_elements);
-    std::vector<KeyT> h_query(num_queries);
-    std::vector<ValueT> h_result_gt(num_queries);
-    std::vector<ValueT> h_result(num_queries);
-
+    std::vector<KeyT> h_key(num_elems);
+    std::vector<ValueT> h_value(num_elems);
     const int64_t seed = 1;
     std::mt19937 rng(seed);
-    std::vector<uint32_t> index(num_elements);
+    std::vector<uint32_t> index(num_elems);
     std::iota(index.begin(), index.end(), 0);
     std::shuffle(index.begin(), index.end(), rng);
-
     for (int32_t i = 0; i < index.size(); i++) {
         h_key[i] = index[i];
         h_value[i] = f(h_key[i]);
     }
 
-    /** Generate key-value pairs for query **/
+    /******** Query data (part from first half, part from second half) *******/
+    uint32_t num_queries = num_elems / 2;
+    float existing_ratio = 0.6f;
     uint32_t num_existing = static_cast<uint32_t>(existing_ratio * num_queries);
+    std::vector<KeyT> h_query(num_queries);
+    std::vector<ValueT> h_result_gt(num_queries);
+    std::vector<ValueT> h_result(num_queries);
 
+    /* from the 1st half */
     for (int i = 0; i < num_existing; i++) {
-        h_query[i] = h_key[num_keys - 1 - i];
+        h_query[i] = h_key[num_elems / 2 - 1 - i];
         h_result_gt[i] = f(h_query[i]);
     }
-
+    /* from the 2nd half */
     for (int i = 0; i < (num_queries - num_existing); i++) {
-        h_query[num_existing + i] = h_key[num_keys + i];
+        h_query[num_existing + i] = h_key[num_elems / 2 + i];
         h_result_gt[num_existing + i] = SEARCH_NOT_FOUND;
     }
-
     /* shuffle */
     std::vector<int> q_index(num_queries);
     std::iota(q_index.begin(), q_index.end(), 0);
@@ -95,27 +90,26 @@ int main(int argc, char** argv) {
         std::swap(h_result_gt[i], h_result_gt[q_index[i]]);
     }
 
-    /** Instantiate hash table **/
-    GpuHashTable<KeyT, ValueT> hash_table(num_keys, num_buckets, 0, seed);
 
-    printf("0) num_keys = %d, num_buckets = %d\n", num_keys, num_buckets);
+    /******* Instantiate hash table ********/
+    GpuHashTable<KeyT, ValueT> hash_table(num_elems, num_buckets, 0, seed);
+
+    /****** Insert and query first half ********/
     float build_time =
-            hash_table.Insert(h_key.data(), h_value.data(), num_keys);
-    printf("1) Hash table built in %.3f ms (%.3f M elements/s)\n", build_time,
-           double(num_keys) / (build_time * 1000.0));
+            hash_table.Insert(h_key.data(), h_value.data(), num_elems / 2);
+    printf("1) Insert finished in %.3f ms (%.3f M elements/s)\n", build_time,
+           double(num_elems / 2) / (build_time * 1000.0));
 
+    /* Expect 0.6 of them found */
     float search_time =
             hash_table.Search(h_query.data(), h_result.data(), num_queries);
-    printf("2) Hash table (existing ratio %.2f) searched in %.3f ms (%.3f M "
-           "queries/s)\n",
-           existing_ratio, search_time,
-           double(num_queries) / (search_time * 1000.0));
+    printf("2) Query finished in %.3f ms (%.3f M queries/s)\n",
+           search_time, double(num_queries) / (search_time * 1000.0));
 
     bool search_success = true;
     for (int i = 0; i < num_queries; i++) {
         if (h_result_gt[i] != h_result[i]) {
-            printf("### wrong result at index %d: [%d] -> %d, but should be "
-                   "%d\n",
+            printf("### Result at index %d: [%d] -> %d, expected: %d\n",
                    i, h_query[i], h_result[i], h_result_gt[i]);
             search_success = false;
         }
@@ -127,55 +121,28 @@ int main(int argc, char** argv) {
     printf("The load factor is %.2f, number of buckets %d\n", load_factor,
            num_buckets);
 
-    /* Delete everything */
-    float delete_time = hash_table.Delete(h_key.data(), num_queries);
-    printf("3) Hash table deleted in %.3f ms (%.3f M queries/s)\n", delete_time,
-           double(num_queries) / (delete_time * 1000.0));
+    /****** Insert and query second half ********/
+    build_time = hash_table.Insert(
+                    h_key.data() + num_elems / 2,
+                    h_value.data() + num_elems / 2, num_elems / 2);
+    printf("3) Insert finished in %.3f ms (%.3f M elements/s)\n", build_time,
+            double(num_elems / 2) / (build_time * 1000.0));
+
+    /* Expect all of them found */
     search_time =
             hash_table.Search(h_query.data(), h_result.data(), num_queries);
-    printf("4) Hash table (existing ratio %.2f) searched in %.3f ms (%.3f M "
-           "queries/s)\n",
-           existing_ratio, search_time,
-           double(num_queries) / (search_time * 1000.0));
+    printf("4) Query finished in %.3f ms (%.3f M queries/s)\n",
+           search_time, double(num_queries) / (search_time * 1000.0));
     search_success = true;
     for (int i = 0; i < num_queries; i++) {
-        if (SEARCH_NOT_FOUND != h_result[i]) {
-            printf("### wrong result at index %d: [%d] -> %d, but should be "
-                   "%d\n",
-                   i, h_query[i], h_result[i], SEARCH_NOT_FOUND);
-            search_success = false;
-        }
-    }
-    if (search_success) {
-        printf("4) Validation done\n");
-    }
-    load_factor = hash_table.measureLoadFactor();
-    printf("The load factor is %.2f, number of buckets %d\n", load_factor,
-           num_buckets);
-
-    /* Insert everything back */
-    build_time =
-            hash_table.Insert(h_key.data(), h_value.data(), num_keys);
-    printf("5) Hash table built in %.3f ms (%.3f M elements/s)\n", build_time,
-           double(num_keys) / (build_time * 1000.0));
-    search_time =
-            hash_table.Search(h_query.data(), h_result.data(), num_queries);
-    printf("6) Hash table (existing ratio %.2f) searched in %.3f ms (%.3f M "
-           "queries/s)\n",
-           existing_ratio, search_time,
-           double(num_queries) / (search_time * 1000.0));
-
-    search_success = true;
-    for (int i = 0; i < num_queries; i++) {
-        if (h_result_gt[i] != h_result[i]) {
-            printf("### wrong result at index %d: [%d] -> %d, but should be "
-                   "%d\n",
+        if (f(h_query[i]) != h_result[i]) {
+            printf("### Result at index %d: [%d] -> %d, expected: %d\n",
                    i, h_query[i], h_result[i], h_result_gt[i]);
             search_success = false;
         }
     }
     if (search_success) {
-        printf("6) Validation done\n");
+        printf("4) Validation done\n");
     }
 
     load_factor = hash_table.measureLoadFactor();
